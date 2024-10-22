d265da35c48ddae4d541a1113b495b3d24f1e9bb
Bibek1604 <npbibek94@gmail.com>
2024-10-22 23:32:17 +0545

    frontend done now backend

--- frontend/carrental/src/App.jsx
+++ frontend/carrental/src/App.jsx
@@ -3,6 +3,8 @@ import SmoothScrollbarWrapper from '../src/component/scrolling/SmoothScrollbarWr
 import Home from '../src/component/routing/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MapofNepal from '../src/component/firstpage/MapofNepal';
+import Trails from '../src/component/secondpage/Trails';
+
 
 import Contect from '../src/component/secondpage/Context'
 
@@ -15,6 +17,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/map" element={<MapofNepal />} />
           <Route path="/Contect" element={<Contect />} />
+
+          <Route path="/trails" element={<Trails />} />
+
 
         </Routes>
       </SmoothScrollbarWrapper>
