b6206754fa5145f5e23526c7d77c3b271aa68edc
Bibek1604 <npbibek94@gmail.com>
2024-10-28 13:50:10 +0545

    desginadded

--- frontend/carrental/src/component/Thirdpage/Practise.jsx
+++ frontend/carrental/src/component/Thirdpage/Practise.jsx
@@ -2,6 +2,31 @@ import React from 'react'
 
 export default function Practise() {
   return (
-    <div>Practise</div>
+    <div className="min-h-screen bg-gray-50">
+      
+      {/* Crausles Section */}
+      <div className="w-auto mt-40 mb-12 ml-20 mr-20 bg-black shadow-xl h-80">
+        {/* Content can go here */}
+      </div>
+      <div className="flex justify-center mt-12 font-bold text-green-800">
+        Pic: <p className="ml-2 text-gray-600">Supadeurali</p>
+      </div>
+
+      {/* Below Section */}
+      <div className="relative flex items-center justify-center my-16 mx-50">
+
+
+  {/* Dashed line with arrow */}
+  <div className="absolute flex items-center justify-between w-full h-full mx-4">
+    <div className="w-full h-px border-t-4 border-gray-400 border-dashed"></div>
+    <svg className="absolute w-8 h-8 text-gray-600 transform rotate-45 -right-4" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path d="M10 17l5-5-5-5v10z" />
+    </svg>
+  </div>
+
+ 
+</div>
+
+    </div>
   )
 }
